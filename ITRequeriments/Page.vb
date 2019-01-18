0d40c3161c77cf98dda187e3f6c6921f4a117bc3
StakeLol2201 <sanhueza.alvaro19@gmail.com>
2019-01-18 15:06:05 -0300

    Arreglar elements = Browser.Document.All con dato Nothing

--- ITRequeriments/Page.vb
+++ ITRequeriments/Page.vb
@@ -1,21 +1,8 @@
 ﻿Public Class Page
     Dim elements As HtmlElementCollection
+    Dim hilo As System.Threading.Thread
     Private Sub Page_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Browser.Navigate("http://scotiabank.service-now.com/")
-        While Browser.ReadyState = WebBrowserReadyState.Complete
-            elements = Browser.Document.All
-            For Each elemento As HtmlElement In elements
-                If elemento.GetAttribute("name") = "callback_0" Then
-                    elemento.SetAttribute("value", "s5145837")
-                End If
-                If elemento.GetAttribute("name") = "callback_1" Then
-                    elemento.SetAttribute("value", "abcd.1234")
-                End If
-                If elemento.GetAttribute("name") = "callback_2" Then
-                    elemento.InvokeMember("Click")
-                End If
-            Next
-        End While
     End Sub
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
@@ -34,30 +21,43 @@
         MessageBox.Show(Browser.ReadyState)
     End Sub
 
-    Private Sub Browser_DocumentCompleted(sender As Object, e As EventArgs) Handles Browser.DocumentCompleted
+    Private Sub Browser_DocumentCompleted(sender As Object, e As EventArgs) Handles Browser.DocumentTitleChanged
         'If Browser.StatusText = "Loaded" Then
         '       MessageBox.Show("La pagina cargó")
         'End If
-        If Browser.ReadyState = WebBrowserReadyState.Complete Then
-            MessageBox.Show(Browser.ReadyState)
-            elements = Browser.Document.All
-            For Each elemento As HtmlElement In elements
-                If elemento.GetAttribute("name") = "callback_0" Then
-                    elemento.SetAttribute("value", "s5145837")
+        If Browser.DocumentTitle.Equals("Esta página no se puede mostrar") Then
+            MessageBox.Show("La página sufrió una caída temporal", "El programa se cerrará de manera inmediata" _
+                            , MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Application.Exit()
+        ElseIf Browser.DocumentTitle.Equals("BNS Employee Login") Then
+            If Browser.ReadyState = WebBrowserReadyState.Interactive Then
+                elements = Browser.Document.All
+                If Browser.StatusText = "Finalizado" Then
+                    For Each elemento As HtmlElement In elements
+                        If elemento.GetAttribute("name") = "callback_0" Then
+                            elemento.SetAttribute("value", "s5145837")
+                        End If
+                        If elemento.GetAttribute("name") = "callback_1" Then
+                            elemento.SetAttribute("value", "abcd.1234")
+                        End If
+                        If elemento.GetAttribute("name") = "callback_2" Then
+                            elemento.InvokeMember("click")
+                        End If
+                    Next
                 End If
-                If elemento.GetAttribute("name") = "callback_1" Then
-                    elemento.SetAttribute("value", "abcd.1234")
-                End If
-                If elemento.GetAttribute("name") = "callback_2" Then
-                    elemento.InvokeMember("click")
-                End If
-            Next
+            End If
         End If
+
+
 
     End Sub
 
     Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
         Browser.Refresh()
     End Sub
+
+    Public Function startPage()
+
+    End Function
 
 End Class
