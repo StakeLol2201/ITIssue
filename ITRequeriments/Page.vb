8a4bd70865d823853bf442e4f7d82ccb9d3a3888
StakeLol2201 <sanhueza.alvaro19@gmail.com>
2019-01-18 11:02:35 -0300

    Upload Project

--- ITRequeriments/Page.vb
+++ ITRequeriments/Page.vb
@@ -18,20 +18,21 @@
         End While
     End Sub
 
-    'Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-    '   elements = Browser.Document.All
-    'For Each elemento As HtmlElement In elements
-    'If elemento.GetAttribute("name") = "callback_0" Then
-    '           elemento.SetAttribute("value", "s5145837")
-    'End If
-    'If elemento.GetAttribute("name") = "callback_1" Then
-    '           elemento.SetAttribute("value", "abcd.1234")
-    'End If
-    'If elemento.GetAttribute("name") = "callback_2" Then
-    '           elemento.InvokeMember("click")
-    'End If
-    'Next
-    'End Sub
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        '   elements = Browser.Document.All
+        'For Each elemento As HtmlElement In elements
+        'If elemento.GetAttribute("name") = "callback_0" Then
+        '           elemento.SetAttribute("value", "s5145837")
+        'End If
+        'If elemento.GetAttribute("name") = "callback_1" Then
+        '           elemento.SetAttribute("value", "abcd.1234")
+        'End If
+        'If elemento.GetAttribute("name") = "callback_2" Then
+        '           elemento.InvokeMember("click")
+        'End If
+        'Next
+        MessageBox.Show(Browser.ReadyState)
+    End Sub
 
     'Private Sub Browser_DocumentCompleted(sender As Object, e As EventArgs) Handles Browser.Navigated
     'If Browser.StatusText = "Loaded" Then
