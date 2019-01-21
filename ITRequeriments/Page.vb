d1fd9a6979dc2f551fa6fb97b24104e8c1f9caff
StakeLol2201 <sanhueza.alvaro19@gmail.com>
2019-01-21 13:40:09 -0300

    Updates 21-01

--- ITRequeriments/Page.vb
+++ ITRequeriments/Page.vb
@@ -1,10 +1,14 @@
 ﻿Public Class Page
     Dim elements As HtmlElementCollection
     Dim hilo As System.Threading.Thread
+    Dim _timer As System.Timers.Timer
     Private Sub Page_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _timer = New Timers.Timer
+        _timer.Interval = 60000 '2 seconds
+        _timer.Enabled = True
+        _timer.Start()
         Browser.Navigate("http://scotiabank.service-now.com/")
     End Sub
-
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         elements = Browser.Document.All
         For Each elemento As HtmlElement In elements
@@ -20,7 +24,6 @@
         Next
         MessageBox.Show(Browser.ReadyState)
     End Sub
-
     Private Sub Browser_DocumentCompleted(sender As Object, e As EventArgs) Handles Browser.DocumentTitleChanged
         'If Browser.StatusText = "Loaded" Then
         '       MessageBox.Show("La pagina cargó")
@@ -30,32 +33,24 @@
                             , MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             Application.Exit()
         ElseIf Browser.DocumentTitle.Equals("BNS Employee Login") Then
-            If Browser.ReadyState = WebBrowserReadyState.Interactive Then
-                Do
-                    elements = Browser.Document.All
-                Loop While 
-                If Browser.StatusText = "Finalizado" Then
-                    For Each elemento As HtmlElement In elements
-                        If elemento.GetAttribute("name") = "callback_0" Then
-                            elemento.SetAttribute("value", "s5145837")
-                        End If
-                        If elemento.GetAttribute("name") = "callback_1" Then
-                            elemento.SetAttribute("value", "abcd.1234")
-                        End If
-                        If elemento.GetAttribute("name") = "callback_2" Then
-                            elemento.InvokeMember("click")
-                        End If
-                    Next
-                End If
+            If Browser.StatusText = "Finalizado" Then
+                elements = Browser.Document.All
+                For Each elemento As HtmlElement In elements
+                    If elemento.GetAttribute("name") = "callback_0" Then
+                        elemento.SetAttribute("value", "s5145837")
+                    End If
+                    If elemento.GetAttribute("name") = "callback_1" Then
+                        elemento.SetAttribute("value", "abcd.1234")
+                    End If
+                    If elemento.GetAttribute("name") = "callback_2" Then
+                        elemento.InvokeMember("click")
+                    End If
+                Next
             End If
         End If
-
-
-
     End Sub
-
     Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
-        Browser.Refresh()
+        'Browser.Refresh()
+        MessageBox.Show("Tara")
     End Sub
-
 End Class
